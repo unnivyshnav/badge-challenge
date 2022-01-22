3df4aaa15d229fa26e61e3f2fead13f4f591dc76
unnivyshnav <79002656+unnivyshnav@users.noreply.github.com>
2022-01-22 17:56:06 +0530

    first

---
+++ src/Body.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css'
+
+
+
+
+export default function Body(props) {
+
+    const [style, setStyle] = useState("dot");
+    const [OnOff,setOnOff] = useState("OFF")
+  
+     const changeStyle = () => {
+        console.log("you just clicked");
+        setStyle(style === "dot" ? "dotOn" : "dot");
+        setOnOff(OnOff === "OFF" ? "ON" : "OFF");
+        console.log(props)
+     }
+
+     const [style1, setStyle1] = useState("dot");
+     const [OnOff1,setOnOff1] = useState("OFF")
+   
+      const changeStyle1 = () => {
+         console.log("you just clicked");
+         setStyle1(style1 === "dot" ? "dotOn" : "dot");
+         setOnOff1(OnOff1 === "OFF" ? "ON" : "OFF");
+      }
+
+  return <div class="grid-container">
+  <div class="grid-item"><div className={style}></div></div>
+  <div class="grid-item"><div className={style1}></div></div>
+  <div class="grid-item"><button className='button' onClick={changeStyle}>{OnOff}</button></div>
+  <div class="grid-item"><button className='button' onClick={changeStyle1}>{OnOff1}</button></div>
+  
+</div>
+}
